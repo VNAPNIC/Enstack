efc623366d137ee22fbbbc05330d5f6de99fe897
vnapnic <vn.apnic@gmail.com>
2021-07-20 00:06:34 +0700

    fixed route forget password

--- lib/manifest.dart
+++ lib/manifest.dart
@@ -1,5 +1,7 @@
+import 'package:enstack/ui/forget_pasword/forget_password_route.dart';
 import 'package:enstack/ui/login/login_route.dart';
 
 void generateRoutes() {
   LoginRoute();
+  ForgetPasswordRoute();
 }
